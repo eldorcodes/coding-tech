75687a67a80a2507689606324e34aa2e0cedbfee
Eldor Togaymurotov <eldorcodes@gmail.com>
2021-10-12 21:03:35 -0400

    done

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const favicon = require('express-favicon')
+const path = require('path')
+
+const app = express()
+
+const port = process.env.PORT || 5000;
+
+app.use(favicon(__dirname + '/build/favicon.ico'))
+app.use(express.static(__dirname))
+app.use(express.static(path.join(__dirname,'build')))
+
+app.get('/*',(req,res) => {
+    res.sendFile(path.join(__dirname,'build','index.html'))
+})
+app.listen(port,(err) => {
+    console.log(`Server is up on port ${port}`);
+})
