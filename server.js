b9aa86677b5acd006f290d0d87248c13e93c3683
Eldor Togaymurotov <eldorcodes@gmail.com>
2021-10-12 22:05:21 -0400

    done

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(favicon(__dirname + '/build/favicon.ico'))
 app.use(express.static(__dirname))
 app.use(express.static(path.join(__dirname,'build')))
 
-app.get('/*',(req,res) => {
+app.get('*',(req,res) => {
     res.sendFile(path.join(__dirname,'build','index.html'))
 })
 app.listen(port,(err) => {
